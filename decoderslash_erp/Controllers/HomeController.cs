cec5d3cd1da1e48d0eb40e544b3fb143801eac06
DeathNet123 <arslan.computer2002@gmail.com>
2023-01-25 21:06:11 +0500

    structure setting updated

--- decoderslash_erp/Controllers/HomeController.cs
+++ decoderslash_erp/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace decoderslash_erp.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            Console.WriteLine($"{_logger}");
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
