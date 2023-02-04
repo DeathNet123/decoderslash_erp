db8292e60038ccad9f4e2124b9fffe586bd59376
DeathNet123 <arslan.computer2002@gmail.com>
2023-02-04 21:21:07 +0500

    Added some Comments

--- decoderslash_erp/Models/CardModelLeft.cs
+++ decoderslash_erp/Models/CardModelLeft.cs
@@ -2,17 +2,17 @@
 {
     public class CardModelLeft
     {
-        public int Counter { get; set; }
-        public String? Tag { get; set; }
+        public int Counter { get; set; } //Numeric Value
+        public String? Tag { get; set; } //Main Tag
 
-        public String? Tag2 { get; set; }
-        public String? Icon { get; set; }
-        public String? Types { get; set; }
+        public String? Tag2 { get; set; } //sub tag...
+        public String? Icon { get; set; } //Icon data of fontawesome..
+        public String? Types { get; set; } //Left, Right, Progress, Large..
 
-        public String? Controller { get; set; }
+        public String? Controller { get; set; } //asp-controller
 
-        public String? Action { get; set; }
+        public String? Action { get; set; } //asp-action...
 
-        public String? Volume { get; set; }
+        public String? Volume { get; set; } //This one is only for the Progress card... 
     }
 }
