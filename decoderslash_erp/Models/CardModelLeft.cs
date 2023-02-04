fa04d19f6ec382d8a452a278fdd745f1a0ccbdc6
DeathNet123 <arslan.computer2002@gmail.com>
2023-02-04 18:16:20 +0500

    Added all of the cards

--- decoderslash_erp/Models/CardModelLeft.cs
+++ decoderslash_erp/Models/CardModelLeft.cs
@@ -4,11 +4,15 @@
     {
         public int Counter { get; set; }
         public String? Tag { get; set; }
+
+        public String? Tag2 { get; set; }
         public String? Icon { get; set; }
         public String? Types { get; set; }
 
         public String? Controller { get; set; }
 
         public String? Action { get; set; }
+
+        public String? Volume { get; set; }
     }
 }
