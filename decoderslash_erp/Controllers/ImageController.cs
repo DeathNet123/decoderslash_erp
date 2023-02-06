8ab81a6a8adf908d358cb718c51e8cb797f398d0
DeathNet123 <arslan.computer2002@gmail.com>
2023-02-06 20:44:01 +0500

    Niceone

---
+++ decoderslash_erp/Controllers/ImageController.cs
@@ -0,0 +1,31 @@
+﻿using decoderslash_erp.Data;
+using decoderslash_erp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace decoderslash_erp.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly decoderslash_erpContext _context;
+        public ImageController(decoderslash_erpContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Avatar()
+        {
+            if (HttpContext.Session.GetString("Cred") == null)
+                return RedirectToAction("Index", "Home");
+            Employee emp = JsonSerializer.Deserialize<Employee>(HttpContext.Session.GetString("Data")!)!;
+            String? AvatarPath = emp.AvatarPath;
+            if(AvatarPath == null)
+            {
+                AvatarPath = "images.png";
+            }
+            String realPath = Path.Combine(Environment.GetEnvironmentVariable("Volume")!, AvatarPath!);
+            FileStream fs = new FileStream(realPath, FileMode.Open, FileAccess.Read);
+            return File(fs, "image/png");
+        }
+    }
+}
---
+++ decoderslash_erp/Migrations/20230206081856_Complex.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace decoderslasherp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Complex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           /* migrationBuilder.CreateTable(
+                name: "Credentials",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credentials", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Designation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BasicSalary = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    FuelAllowence = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    Housing = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    OverTimeHourlyRate = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    JoiningDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CredentialsID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });*/
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Credentials");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
