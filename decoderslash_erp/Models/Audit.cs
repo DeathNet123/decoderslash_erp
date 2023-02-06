409fc6c63d7402fbb74b1dc217fa1ec40283d72c
DeathNet123 <arslan.computer2002@gmail.com>
2023-02-06 23:35:07 +0500

    AuditClass

---
+++ decoderslash_erp/Models/Audit.cs
@@ -0,0 +1,10 @@
+﻿namespace decoderslash_erp.Models
+{
+    public class Audit
+    {
+        public int UserAdd { get; set; }
+        public int UserDel { get; set; }
+        public int UserMod { get; set; }
+        public bool isActive { get; set; }
+    }
+}
